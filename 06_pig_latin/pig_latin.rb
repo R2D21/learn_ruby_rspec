0a549c82d529b890189f50ddbb4db0f63ac79845
remi <root@localhost.localdomain>
2018-07-09 21:05:58 +0200

    third commit

--- 06_pig_latin/pig_latin.rb
+++ 06_pig_latin/pig_latin.rb
@@ -1,1 +1,15 @@
 #write your code here
+
+def translate(string)
+  voyelles = "aeiouy"
+  string_copy = string
+
+  voyelles.each_char do | objet |
+    if (string[0] ==  objet)
+        string_copy += "ay"
+  end
+end
+return (string_copy)
+end
+
+puts translate("apple")
